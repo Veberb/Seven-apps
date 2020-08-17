0ce52760a8a78e8f93ad08773052b2f1c11e9aab
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2020-08-17 09:23:44 -0300

    query to get/list users

--- src/modules/user/user.resolvers.js
+++ src/modules/user/user.resolvers.js
@@ -3,6 +3,27 @@ const { UserInputError } = require('apollo-server');
 const User = require('./user.schema');
 
 const resolvers = {
+  Query: {
+    getUser: (parent, { id }) => {
+      return User.findById(id);
+    },
+    listUsers: async (parent, { user = {}, offset = 1, limit = 10 }) => {
+      const query = {};
+      const { name, email } = user;
+
+      if (name) query.name = new RegExp(name);
+      if (email) query.email = new RegExp(email);
+
+      const [users, totalUsers] = await Promise.all([
+        User.find(query)
+          .skip((offset - 1) * limit)
+          .limit(limit),
+        User.countDocuments(query),
+      ]);
+
+      return { users, totalUsers };
+    },
+  },
   Mutation: {
     createUser: async (parent, { user }) => {
       const foundUser = await User.countDocuments({ name: user.name });
--- src/modules/user/user.types.js
+++ src/modules/user/user.types.js
@@ -5,8 +5,18 @@ const typeDefs = gql`
     createUser(user: UserInput!): User!
   }
 
+  extend type Query {
+    getUser(id: String!): User
+    listUsers(user: UserInput, offset: Int, limit: Int): UserPagination
+  }
+
+  type UserPagination {
+    users: [User]
+    totalUsers: Int
+  }
+
   input UserInput {
-    name: String!
+    name: String
     email: String
   }
 
